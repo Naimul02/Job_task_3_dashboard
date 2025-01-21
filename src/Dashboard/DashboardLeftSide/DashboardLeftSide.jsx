b45819480152623e457da2d34e85b3c37b22dd8f
Naimul02 <naimulislum39@gmail.com>
2025-01-20 19:20:06 -0800

    pie chart added

--- src/Dashboard/DashboardLeftSide/DashboardLeftSide.jsx
+++ src/Dashboard/DashboardLeftSide/DashboardLeftSide.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const DashboardLeftSide = () => {
     return (
         <div>
-           <h1 className="text-xl font-semibold">Left Side</h1>
+           <div>
+            <img src="https://i.pinimg.com/474x/95/70/d1/9570d17fa556c18cd9ca616ce7c44ad1.jpg"className="w-24 h-24 rounded-full" alt="" />
+           </div>
 
            
         </div>
